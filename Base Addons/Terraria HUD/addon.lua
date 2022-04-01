76954e92e734102fb7d3a3292af08a8d2e6d645c
offyerrocker <offyerrocker88@gmail.com>
2022-04-01 03:23:00 -0700

    Terraria Hotbar addon
    Ported Terraria Hotbar from Minimalist HUD Pro (static image only, no functionality)

---
+++ Base Addons/Terraria HUD/addon.lua
@@ -0,0 +1,20 @@
+return "terraria",{
+	name = "Terraria Hotbar",
+	desc = "Item Hotbar from Terraria",
+	autodetect_assets = true,
+	layer = -1,
+	categories = {
+		"misc" --nonfunctional; static image only
+	},
+	texture = "guis/textures/mhudu/terraria_hotbar",
+	create_func = function(addon,parent_panel)
+		local bitmap = parent_panel:bitmap({
+			name = "bitmap",
+			layer = 1,
+			texture = addon.texture,
+			x = 500,
+			y = 80
+		})
+		addon.bitmap = bitmap
+	end
+}
